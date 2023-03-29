f3fdfbe09ffa1116b45aa33304837a6820ce299a
Albert Brox <albert@exypno.tech>
2023-03-29 00:38:20 -0700

    improve syntax highlighting

--- formatter.py
+++ formatter.py
@@ -3,35 +3,13 @@ import re
 import shutil
 import sys
 
-import logging
-import logging.config
-
-logging_config = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file_handler': {
-            'class': 'logging.FileHandler',
-            'filename': '/tmp/formatter.log',
-            'mode': 'w',
-            'formatter': 'my_formatter',
-        },
-    },
-    'formatters': {
-        'my_formatter': {
-            'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-        },
-    },
-    'loggers': {
-        '': {
-            'handlers': ['file_handler'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
-
-logging.config.dictConfig(logging_config)
+# ANSI escape codes
+RESET = '\033[0m'
+STRING_COLOR = '\033[1;32m'
+NEUTRAL_COLOR = '\033[1;30m'
+KEYWORD_COLOR = '\033[1;93m'
+VALUE_COLOR = '\033[1;31m'
+TYPE_COLOR = '\033[1;36m'
 
 class TokenFormatter:
     def __init__(self):
@@ -44,6 +22,9 @@ class TokenFormatter:
 
         self.consecutive_ticks = 0
         self.last_was_tick = False
+        self.prev_token = ""
+
+        self.operators = ['+', '-', '*', '/', '%', '//', '**', '==', '!=', '<', '>', '<=', '>=', '&', '|', '^', '~', '<<', '>>']
 
     def _update_terminal_width(self):
         width, _ = shutil.get_terminal_size()
@@ -89,6 +70,8 @@ class TokenFormatter:
         else:
             self.print_token(token)
 
+        self.prev_token = token
+
     def print_token(self, token):
         if self.current_line_len + len(token) + 1 > self.terminal_width:
             print()
@@ -106,33 +89,66 @@ class TokenFormatter:
         sys.stdout.write(ht)
         sys.stdout.flush()
 
-    def syntax_highlight(self, token):
-        highlighted_token = token
-        set_in_string = False
-
+    def is_keyword(self, token):
         keywords = set(['if', 'else', 'for', 'while', 'return', 'class',
             'def', 'function', 'import', 'from', 'export', 'const',
             'let', 'var', 'switch', 'case', 'try', 'catch', 'finally',
             'break', 'continue', 'with', 'as', 'in', 'match', 'case',
+            'elif',
         ])
+
+        return token in keywords
+
+    def is_typeval(self, token):
+        types_and_values = set(['int', 'float', 'str', 'bool',
+            'None', 'True', 'False','complex', 'tuple', 'list',
+            'dict', 'set', 'frozenset', 'bytes', 'bytearray',
+            'memoryview', 'range', 'enumerate', 'len',
+        ])
+
+        result = token in types_and_values
+        result = result or token + '(' in types_and_values
+        return result
+
+    def is_digit(self, token):
+        prev = self.prev_token
+        try:
+            last_char = prev[-1]
+        except:
+            last_char = None
+
+        result = token.isdigit()
+        result = result and \
+                (prev.strip().isdigit() \
+                or last_char in ' ({[' \
+                or prev.strip() in self.operators)
+
+        return result
+
+    def syntax_highlight(self, token):
+        highlighted_token = token
+        set_in_string = False
 
         s_token = token.strip()
 
-        if s_token in keywords and not self.in_string and not self.in_comment:
-            highlighted_token = '\033[1;36m' + token + '\033[0m'
+        if self.is_keyword(s_token) and not self.in_string and not self.in_comment:
+            highlighted_token = KEYWORD_COLOR + token + RESET
+
+        if self.is_typeval(s_token) and not self.in_string and not self.in_comment:
+            highlighted_token = TYPE_COLOR + token + RESET
 
         comment_start = re.compile('\/\/|\/\*|\#')
 
         if comment_start.match(s_token) and not self.in_string:
             self.in_comment = True
-            highlighted_token = '\033[1;30m' + token + '\033[0m'
+            highlighted_token = NEUTRAL_COLOR + token + RESET
 
         if self.in_comment and '\n' in token:
             self.in_comment = False
 
         if self.in_comment and s_token.endswith('*/'):
             self.in_comment = False
-            highlighted_token = '\033[1;30m' + token + '\033[0m'
+            highlighted_token = NEUTRAL_COLOR + token + RESET
 
         string_start = re.compile('\"|\'')
         string_match = string_start.search(s_token)
@@ -140,13 +156,7 @@ class TokenFormatter:
             self.in_string = True
             self.string_delimiter = string_match[0]
             delimiter_index = token.index(self.string_delimiter)
-            logging.debug('in the opening branch')
-            logging.debug(f'token = <{token}>')
-            logging.debug(f'delim_i = {delimiter_index}')
-            highlighted_token = token[:delimiter_index] +'\033[1;32m' + token[delimiter_index:] +'\033[0m'
-            logging.debug(f'upto = <{token[:delimiter_index]}>')
-            logging.debug(f'past = <{token[delimiter_index:]}>')
-            logging.debug(f'hi_token = <{highlighted_token}>')
+            highlighted_token = token[:delimiter_index] + STRING_COLOR + token[delimiter_index:] + RESET
             if len(string_start.findall(s_token)) != 2 or token[delimiter_index - 1] =='\\':
                 set_in_string = True
 
@@ -154,24 +164,14 @@ class TokenFormatter:
             delimiter_index = token.index(self.string_delimiter)
             if token[delimiter_index - 1] != '\\':
                 self.in_string = False
-                highlighted_token ='\033[1;32m' + token[:delimiter_index+1] +'\033[0m' + token[delimiter_index+1:]
+                highlighted_token = STRING_COLOR + token[:delimiter_index+1] + RESET + token[delimiter_index+1:]
             else:
-                highlighted_token ='\033[1;32m' + token +'\033[0m'
-
-            logging.debug('in the closure branch')
-            logging.debug(f'token = <{token}>')
-            logging.debug(f'delim_i = {delimiter_index}')
-            logging.debug(f'upto = <{token[:delimiter_index+1]}>')
-            logging.debug(f'past = <{token[delimiter_index+1:]}>')
-            logging.debug(f'hi_token = <{highlighted_token}>')
+                highlighted_token = STRING_COLOR + token + RESET
 
         if not set_in_string and self.in_string or self.in_comment:
-            logging.debug('in the current branch')
-            highlighted_token ='\033[1;32m' + token +'\033[0m'
-            logging.debug(f'token = <{token}>')
-            logging.debug(f'hi_token = <{highlighted_token}>')
+            highlighted_token = STRING_COLOR + token + RESET
 
-        if s_token.isdigit() and not self.in_string and not self.in_comment:
-            highlighted_token = '\033[1;31m' + token + '\033[0m'
+        if self.is_digit(s_token) and not self.in_string and not self.in_comment:
+            highlighted_token = VALUE_COLOR + token + RESET
 
         return highlighted_token
